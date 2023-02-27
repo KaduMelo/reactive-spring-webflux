94580bfce67cd8f5df795401c5bf8870814ae518
Kadu Chessi <kaduchessi@gmail.com>
2023-02-27 07:01:25 -0300

    Added handling exceptions to WebClient

--- movies-service/src/main/java/com/reactivespring/exception/ReviewsClientException.java
+++ movies-service/src/main/java/com/reactivespring/exception/ReviewsClientException.java
@@ -2,9 +2,30 @@ package com.reactivespring.exception;
 
 public class ReviewsClientException extends RuntimeException{
     private String message;
+    private Integer statusCode;
 
-    public ReviewsClientException(String message) {
+    public ReviewsClientException(String message, Integer statusCode) {
         super(message);
         this.message = message;
+        this.statusCode = statusCode;
     }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+
 }
